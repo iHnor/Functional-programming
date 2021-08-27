d576d768e5fa4bf2dc8bbdf55ab9dbe6e8eda2b9
Ihor Mykhno <igormykhno@gmail.com>
2021-08-27 18:19:01 +0300

    Максимальной площади из всех черных квадратов

---
+++ index.js
@@ -0,0 +1,26 @@
+let shapes = [
+    { color: 'black', width: 20, height: 20 },
+    { color: 'red', width: 12, height: 15 },
+    { color: 'red', width: 4, height: 13 },
+    { color: 'white', width: 23, height: 34 },
+    { color: 'green', width: 11, height: 11 },
+    { color: 'white', width: 21, height: 9 },
+    { color: 'black', width: 19, height: 19 },
+];
+const flow      = (...functs) => shapes => functs.reduce((accum, func) => func(accum), shapes); 
+const map       = func => squares => squares.map(func);
+const reduce    = (func, value) => squares => squares.reduce(func, value);
+
+const isBlack   = shapes => shapes.color  === 'black';
+const isSquare  = shapes => shapes.height === shapes.width;
+const and       = (func_1, func_2) => shapes => func_1(shapes) && func_2(shapes);
+const filter    = func => shapes  => shapes.filter(func);
+const squareOfFigures = func => func.width * func.height;
+const compSquare = (max, curr) => max > curr ? max : curr;
+
+const maxSquareBlackSquares = flow(
+    filter(and(isBlack, isSquare)),
+    map(squareOfFigures),
+    reduce(compSquare, -1)
+)
+console.log(maxSquareBlackSquares(shapes))
