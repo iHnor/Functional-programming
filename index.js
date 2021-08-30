c10b26e4c2245b8da3696c735ca47ab2e1ef6ae1
iHnor <igormykhno@gmail.com>
2021-08-30 13:44:13 +0300

    final version

--- index.js
+++ index.js
@@ -12,11 +12,19 @@ const map       = func => squares => squares.map(func);
 const reduce    = (func, value) => squares => squares.reduce(func, value);
 
 const isBlack   = shapes => shapes.color  === 'black';
+const isRed     = shapes => shapes.color === 'red';
+
 const isSquare  = shapes => shapes.height === shapes.width;
+const isRectangle = shapes => shapes.height !== shapes.width;
+
 const and       = (func_1, func_2) => shapes => func_1(shapes) && func_2(shapes);
 const filter    = func => shapes  => shapes.filter(func);
+
 const squareOfFigures = func => func.width * func.height;
+const perimeterOfFigures = func => (func.width + func.height) * 2;
+
 const compSquare = (max, curr) => max > curr ? max : curr;
+const sumOfPerimeter = (firstPr, secondPr) => firstPr + secondPr;
 
 const maxSquareBlackSquares = flow(
     filter(and(isBlack, isSquare)),
@@ -24,3 +32,10 @@ const maxSquareBlackSquares = flow(
     reduce(compSquare, -1)
 )
 console.log(maxSquareBlackSquares(shapes))
+
+const sumOfPerimetersRedRectangles = flow (
+    filter(and(isRed, isRectangle)),
+    map(perimeterOfFigures), 
+    reduce(sumOfPerimeter, 0)
+)
+console.log(sumOfPerimetersRedRectangles(shapes));
